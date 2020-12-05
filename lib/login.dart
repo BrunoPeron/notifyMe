f592dcb65d456d86d82b9975cb0097442731b7d4
Bruno <bruno.peron@unochapeco.edu.br>
2020-12-04 21:34:47 -0300

    cadastro de usuario

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:projeto_app/app.dart';
 
 void main() {
   runApp(LoginPage());
@@ -199,13 +200,18 @@ class _LoginPageState extends State<LoginPage>{
                         )
                     ),
                     child: Center(
-                      child: Text('Cadastre-se'.toUpperCase(),//mudar para botão
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold
+                      child: IconButton(
+                        icon: Text('Cadastro'.toUpperCase(),//mudar para botão
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold
+                          ),
                         ),
-                      ),
+                        onPressed: (){
+                          runApp(App());
+                        },
                     ),
+                  ),
                   ),
                 ],
               ),
